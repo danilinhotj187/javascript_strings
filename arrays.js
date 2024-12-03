61041e7811d4ac4500473d81aa36831f54462488
danilinhotj187 <kjdanilo187@gmail.com>
2024-12-03 18:48:36 -0300

    index

---
+++ arrays.js
@@ -0,0 +1,28 @@
+let frutas = ['Maçã', 'Banana', 'Laranja']
+
+console.log(frutas[0])
+console.log(frutas[1])
+console.log(frutas[2])
+
+frutas[1] = 'Manga'
+console.log('\n ---- \n' + frutas[1])
+
+
+console.log(frutas)
+
+console.log('\n ---- \n' + frutas[1])
+console.log('\n ---- \n' + frutas[1])
+
+
+
+
+
+// let cabecalho = ['Código' , 'Nome' , 'Preço']
+// let produto = ['ABC123' , 'Fone de ouvido' , 150.00]
+
+// console.log(
+//     `Detalhes do produto:
+//     ${cabecalho[0]} : ${produto[0]}
+//     ${cabecalho[1]} : ${produto[1]}
+//     ${cabecalho[2]} : ${produto[2]}
+// `)
